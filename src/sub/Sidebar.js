4b65e31986473e15e74905fc02115fa26dbc299c
B Moniz <bcm811@gmail.com>
2020-06-29 17:01:37 -0600

    Added 2 modes, Text, and Image, will have to revisit image about this() lexical context

--- src/sub/Sidebar.js
+++ src/sub/Sidebar.js
@@ -3,7 +3,34 @@ import EdenLogo from './EdenLogo';
 import '../css/Reset.css';
 import '../css/Base.css';
 import '../css/Sidebar.css';
+import '../css/Mode.css';
 
+function TextMode(){
+		return (
+			<div className="text-mode">
+				<label for="project-name">Project Name:</label>
+				<input type="text" name="project-name"/>
+				<label for="text-value">Body:</label>
+				<input type="text" className="text-value" name="text-value" placeholder="Type your project description here"/>
+			</div>
+			);
+}
+
+function ImageMode(props){
+		let thePreview = props.preview;
+		let functionRef = props.ref;
+
+	return (
+		<div className="image-mode">
+			<label for="image">Image</label>
+			<input type="file" placeholder="upload an image" name="image" onChange={functionRef} />
+			{thePreview}
+			<h3>Image Upload Preview is The Broken</h3>
+			<p>Something to do with .bind(this) being actually ()=> function and this now being able to use lexical context, and also React...</p>
+		</div>
+
+		);
+}
 class Sidebar extends React.Component {
 	constructor(props){
 		super(props);
@@ -11,8 +38,29 @@ class Sidebar extends React.Component {
 		this.state = {
 			"mode": 0,
 			"icons": ["Text", "Image", "Video", "Shape", "List/Graphs", "Templates", "Calendar" ],
+			"project": {
+				image:[],
+				name:"",
+				body:""
+			}
 		}
 	}
+	fileChangeHandler(event){
+		this.setState({
+			selectedFile: event.target.files[0]
+		})
+
+		let reader = new FileReader();
+
+		reader.onloadend = () => {
+			this.setState({
+				imagePreviewUrl :reader.result
+			});
+		}
+
+		reader.readAsDataURL(event.target.files[0])
+	}
+
 	render(){
 		const icons = this.state.icons.map(function(icon, index){
 			console.log(index);
@@ -23,6 +71,22 @@ class Sidebar extends React.Component {
 				return <li data-mode={index} onClick={ () => this.setState({mode : index})} >{icon}</li>;
 			}
 		}, this);
+
+		const currentMode = this.state.mode;
+		const previewImageUrl = this.state.imagePreviewUrl;
+		const changeFunction = this.fileChangeHandler;
+		let contentSection;
+		if (currentMode == 0) {
+			contentSection = <TextMode />;
+		}
+		else if (currentMode == 1) {
+			let preview;
+			if (previewImageUrl) {
+				preview = previewImageUrl;
+			}
+			contentSection = <ImageMode ref={changeFunction} preview={preview} />;
+		}
+	
 		return (
 			<div>
 				<div className="sidebar">
@@ -34,6 +98,9 @@ class Sidebar extends React.Component {
 							{icons}
 						</ul>
 					</div>
+				</div>
+				<div className="the-content">
+					{contentSection}
 				</div>
 			</div>
 			);
